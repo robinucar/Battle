b8496fbd3a1310dacf0d1f43ca46dd81e5b1c4f4
robin ucar <ucarmehmet.ch@gmail.com>
2022-01-06 18:27:21 +0000

    setup Battle project

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+    configure :development do
+        register Sinatra::Reloader
+    end
+
+
+
+
+    # Routes
+    get '/' do
+        'Hello Battle!'
+    end
+
+    #Start the server if this file is executed directly (do not change the line below)
+    run! if app_file == $0
+end
