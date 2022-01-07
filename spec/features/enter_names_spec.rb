3699fbdb40a67537bb25bbb7fbc352985da2b214
robin ucar <ucarmehmet.ch@gmail.com>
2022-01-07 15:32:31 +0000

    view hit points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -11,5 +11,14 @@ feature 'Enter names' do
 
         expect(page).to have_content "First player name is Robin\nSecond player name is Kathryn"
     end
+    feature 'hit points' do
+        scenario 'see Player 2 hit points' do
+            visit('/')
+            fill_in :first_player_name, with: 'Robin'
+            fill_in :second_player_name, with: 'Kathryn'
+            click_button 'Submit'
+            expect(page).to have_content 'Kathryn: 60HP'
+      end
+    end
 
 end
